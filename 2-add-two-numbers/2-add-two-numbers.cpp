e4befc35af9eeebc27381250c28071f61bffd9aa
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-09-08 09:59:37 +0530

    Time: 92 ms (5.50%), Space: 71.5 MB (51.53%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -0,0 +1,88 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+     
+        ListNode *head = new ListNode();
+        ListNode *headClone = head;
+        int sum = l1->val + l2->val;
+        bool carry = false;
+        if(sum > 9)
+        {
+            carry = true;
+            headClone->val = sum % 10;
+        }
+        else
+        {
+            headClone->val = sum;
+        }
+        l1 = l1->next;
+        l2 = l2->next;
+        
+        while(l1 && l2)
+        {
+            ListNode* temp = new ListNode();
+            sum = l1->val + l2->val;
+            if(carry)
+            {
+                sum++;
+                carry = false;
+            }
+            if(sum > 9)
+            {
+                carry = true;
+                temp->val = sum % 10;
+            }
+            else
+            {
+                temp->val = sum;
+            }
+            headClone->next = temp;
+            headClone = headClone->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        if(!l1)
+        {
+          l1 = l2;  
+        }
+        while(l1)
+        {
+            ListNode* temp = new ListNode();
+            sum = l1->val;
+            if(carry)
+            {
+                sum++;
+                carry = false;
+            }
+            if(sum > 9)
+            {
+                temp->val = sum % 10;
+                carry = true;
+            }
+            else
+            {
+                temp->val = sum;
+            }
+            headClone->next = temp;
+            headClone = headClone->next;
+            l1 = l1->next;
+        }
+        if(carry)
+        {
+            ListNode *temp = new ListNode();
+            temp->val = 1;
+            headClone->next = temp;
+        }
+        return head;
+    }
+};
