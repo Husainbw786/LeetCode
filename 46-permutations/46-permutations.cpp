900f54a69b49db2216618a6ccb8d79a52b02f6f0
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-09-16 09:18:32 +0530

    Time: 7 ms (41.37%), Space: 8 MB (37.53%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    
+    void permutation(vector<int>& nums, int low , int high)
+    {
+        if( low >= high)
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i = low; i<= high;i++)
+        {
+            swap(nums[low],nums[i]);
+           permutation(nums,low+1,high);
+            swap(nums[low],nums[i]);
+                
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+     
+        permutation(nums,0,nums.size()-1);
+        return ans;
+        
+    }
+};
