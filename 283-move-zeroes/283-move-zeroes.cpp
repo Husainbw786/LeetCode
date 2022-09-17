08367f73191bea0376d243d8ea29d1958488d106
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-09-17 10:04:05 +0530

    Time: 61 ms (17.77%), Space: 19.3 MB (57.74%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-     
-        int len = nums.size() - 1;
+      int i;
         int j = 0;
-        
-        int i;
-        for(i=0;i<=len;i++)
+        int n = nums.size();
+        for(i=0;i<n;i++)
         {
             if(nums[i] != 0)
             {
@@ -14,9 +12,10 @@ public:
                 j++;
             }
         }
-        for(i=j;i<=len;i++)
+        for(i=j;i<n;i++)
         {
             nums[i] = 0;
         }
+        
     }
 };
