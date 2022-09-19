daaaa67f3c676b2964c6467027ef910cad0c47cf
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-09-19 08:51:22 +0530

    Time: 69 ms (9.19%), Space: 17 MB (53.64%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,22 +2,12 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
      
-        sort(nums.begin(),nums.end());
-        int i = 0;
-        int j =1;
-        int n = nums.size();
-        while(j<n)
+        int ans = 0;
+        int i;
+        for(i=0;i<nums.size();i++)
         {
-            if(nums[i] == nums[j])
-            {
-                i += 2;
-                j += 2;
-            }
-            else
-            {
-                return nums[i];
-            }
+            ans = ans^nums[i];
         }
-        return nums[n-1];
+        return ans;
     }
 };
