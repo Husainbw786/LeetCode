27dc27f2f79f6ab1c9d1c4e42bc8f64bc118d42a
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-09-28 23:27:42 +0530

    Time: 32 ms (37.79%), Space: 15.9 MB (70.23%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -5,23 +5,27 @@ public:
         int n = letters.size();
         int high = n-1;
         int low = 0;
-        char ans = ' ';
+        char ans;
+        if(letters[high] == target || letters[high] < target)
+        {
+            return letters[low];
+        }
         while(low <= high)
         {
             int  mid  = low + (high - low)/2;
-            if(letters[mid] > target)
+            if(letters[mid] == target && letters[mid+1] != target)
             {
-                ans = letters[mid];
+                return letters[mid+1];
+            }
+            else if(letters[mid] > target)
+            {
                 high = mid - 1;
+                ans = letters[mid];
             }
             else 
             {
                 low = mid + 1;
             }
-        }
-        if(ans==' ')
-        {
-            return letters[0];
         }
         return ans;
         
