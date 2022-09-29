cb3f53084707c55d66c91dc51d2d0024194aa618
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-09-29 20:24:47 +0530

    Time: 12 ms (8.47%), Space: 8.4 MB (55.42%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        
+        unordered_set<int>st;
+        int i;
         int n = nums.size();
-        int i,j;
-        int count = 0;
         for(i=0;i<n;i++)
         {
-            if(nums[i] == 0)
+            if(nums[i] > 0)
             {
-                continue;
+                st.insert(nums[i]);
             }
-            int x = nums[i];
-            for(j = 0;j<n;j++)
-            {
-                if(nums[j] > 0)
-                {
-                    nums[j] = nums[j] - x;
-                }
-            }
-            count++;
         }
-        return count;
-        
+        return st.size();
     }
 };
