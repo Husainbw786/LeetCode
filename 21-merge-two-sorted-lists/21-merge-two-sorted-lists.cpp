0a3a3c72373d75bdaa7311d0ad3da77ba9650481
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-10-06 17:57:53 +0530

    Time: 23 ms (8.77%), Space: 15 MB (14.12%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,51 +11,54 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+       
+        ListNode* head;
+        ListNode* headClone;
         
-        ListNode* head,*headClone;
-        if(!list1)
+        if(list1 == NULL)
         {
             return list2;
         }
-        else if (!list2)
+        else if(list2 == NULL)
         {
             return list1;
         }
-        if(list2->val > list1->val)
-        {
-            head = new ListNode(list1->val);
-            list1 = list1->next;
-        }
-        else
+        if(list1->val > list2->val)
         {
             head = new ListNode(list2->val);
             list2 = list2->next;
         }
-        headClone = head;
-        while(list1 && list2)
+        else
         {
-            if(list2->val > list1->val)
-            {
-                headClone->next = new ListNode(list1->val);
-                list1 = list1->next;
-            }
-            else
+            head = new ListNode(list1->val);
+            list1 = list1->next;
+        }
+        headClone = head;
+        while(list1 != NULL && list2 != NULL)
+        {
+            if(list1->val > list2->val)
             {
                 headClone->next = new ListNode(list2->val);
                 list2 = list2->next;
             }
-            headClone = headClone->next;
+            else
+            {
+                 headClone->next = new ListNode(list1->val);
+                 list1 = list1->next;
+            }
+            headClone  = headClone->next;
         }
-        if(!list1)
+        if(list1 == NULL)
         {
             list1 = list2;
         }
-        while(list1)
+        while(list1 != NULL)
         {
             headClone->next = new ListNode(list1->val);
             list1 = list1->next;
             headClone = headClone->next;
         }
         return head;
+       
     }
 };
