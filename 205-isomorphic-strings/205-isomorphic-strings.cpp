66f56385c6b26c68e30d9b07cb4e83e1d8dcfc23
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-10-06 10:09:03 +0530

    Time: 14 ms (44.25%), Space: 7 MB (73.24%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int count_arr1[256] = {0};
-        int count_arr2[256] = {0};
+        unordered_map<char,char>mp1;
+        unordered_map<char,char>mp2;
         int i;
         int n = s.size();
         for(i=0;i<n;i++)
         {
-            if(! count_arr1[s[i]] &&  !count_arr2[t[i]])
+            if(! mp1[s[i]] &&  !mp2[t[i]])
             {
-            count_arr1[s[i]] = t[i];
-            count_arr2[t[i]] = s[i];
+            mp1[s[i]] = t[i];
+            mp2[t[i]] = s[i];
             }
             else
             {
-                if(count_arr1[s[i]] != t[i])
+                if(mp1[s[i]] != t[i])
                 {
                     return false;
                 }
