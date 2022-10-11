712f5dc42d455b38cef847c54d1a2f4fca5d5beb
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-10-11 23:57:11 +0530

    Time: 21 ms (71.89%), Space: 12.1 MB (8.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int i;
         unordered_map<int,int>mp;
-        vector<int>v;
+        vector<int>v(2,0);
         for(i=0;i<nums.size();i++)
         {
             int val = target - nums[i];
             if(mp.find(val) != mp.end())
             {
-                v.push_back(mp[target-nums[i]]);
-                v.push_back(i);
+                v[0] = mp[target-nums[i]];
+                v[1] = i;
             }
             else
             {
