e1760c93faa2a6f71cdb46843411e9d2026a894e
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-10-13 01:18:24 +0530

    Time: 2284 ms (6.49%), Space: 183.7 MB (7.72%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,6 +5,7 @@ public:
         set<vector<int>>st;
         vector<vector<int>>v;
         int n = nums.size();
+        vector<int>temp;
         sort(nums.begin(),nums.end());
         int i,j,k;
         for(i=0;i< n-2;i++)
@@ -16,7 +17,11 @@ public:
                 int sum = nums[i] + nums[j] + nums[k];
                 if( sum == 0)
                 {
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(nums[k]);
                     st.insert({nums[i],nums[j],nums[k]});
+                    temp.clear();
                     j++,k--;
                 }
                 else if(sum > 0)
