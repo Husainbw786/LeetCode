7a2a4c2fb40e444931accaee3ebbb12d657cadb8
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-10-16 11:03:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.21%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        int climb = 1;
+        int a = 1;
+        int b = 1;
+        for(int i=1;i<n;i++)
+        {
+            climb = a + b;
+            a = b;
+            b = climb;
+        }
+        return climb;
+    }
+};
