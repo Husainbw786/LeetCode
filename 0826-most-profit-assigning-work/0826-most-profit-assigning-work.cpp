a452df422ca36b57792a40c7fa284daa0bab9d6b
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-10-20 18:29:39 +0530

    Time: 179 ms (40.89%), Space: 36.3 MB (85.63%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
      int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        vector<pair<int, int>> vec(difficulty.size());
-        int i, j,size, maxProfit = 0,  maxProfitSoFar=0;
+        vector<pair<int, int>>v(difficulty.size());
+        int i, j;
+        int size; 
+        int maxProfit = 0, maxProfitSoFar=0;
         size = difficulty.size();
         for(i=0; i<size; i++)
-            vec[i] = {difficulty[i], profit[i]};
-    
-        sort(vec.begin(), vec.end());
+        {
+            v[i] = {difficulty[i], profit[i]}; 
+        }
+        sort(v.begin(),v.end());
         sort(worker.begin(), worker.end());
      
-        auto it = vec.begin();
-        for(i=0; i<worker.size(); i++)
+        auto it = v.begin();
+        for(i=0;i<worker.size();i++)
         {
-            while(it!=vec.end() && it->first <= worker[i])
+            while(it!=v.end() && it->first <= worker[i])
             {
                 maxProfitSoFar = max(maxProfitSoFar, it->second);
                 it++;
