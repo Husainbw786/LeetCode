4c57d1d40b6c0743ab1cc657e6055f8983361b37
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-10-25 17:47:13 +0530

    Time: 188 ms (27.64%), Space: 44.8 MB (24.83%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -4,15 +4,15 @@ public:
         
         int size = nums.size();
         int i;
-        unordered_map<int,int>mp;
+        unordered_set<int>st;
         for(i=0;i<size;i++)
         {
-            mp[nums[i]]++;
+            st.insert(nums[i]);
         }
         vector<int>ans;
         for(i=1;i<=size;i++)
         {
-            if(mp.find(i) == mp.end())
+            if(st.find(i) == st.end())
             {
                 ans.push_back(i);
             }
