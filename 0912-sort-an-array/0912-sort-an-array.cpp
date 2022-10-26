375bee1062cc826d82b6f9e246bf870cfe2d522e
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-10-26 21:01:09 +0530

    Time: 259 ms (62.01%), Space: 57.2 MB (68.82%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -3,11 +3,12 @@ public:
     vector<int> sortArray(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         // BUBBLE SORT 
-        /*
+        
         int i,j;
         int n = nums.size();
         for(i=0;i<n-1;i++)
         {
+            bool flag = false;
             for(j=0;j<n-i-1;j++)
             {
                 if(nums[j] > nums[j+1])
@@ -15,11 +16,15 @@ public:
                     int temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
+                    flag = true;
                 }
+            }
+            if(flag == false)
+            {
+                break;
             }
         }
         return nums;
-        */
-        return nums;
+        
     }
 };
