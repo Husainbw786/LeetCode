60c44e678767b517bb702b0133521cf0fe933e90
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-11-06 09:26:14 +0530

    Create 713. Subarray Product Less Than K.cpp

---
+++ 713. Subarray Product Less Than K.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        
+        int start = 0;
+        int end = 0;
+        int n = nums.size();
+        int count = 0;
+        long long p = 1;
+        while(end < n)
+        {
+            p *= nums[end];
+            while(start < n && p >= k)
+            {
+                p = p / nums[start];
+                start++;
+            }
+            if(p < k)
+            {
+                count += end - start + 1;
+            }
+            end ++;
+        }
+        return count;
+    }
+};
