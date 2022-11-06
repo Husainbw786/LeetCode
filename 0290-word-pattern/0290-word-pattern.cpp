cbb768bd90b024e256a130a5f82f9de1714e4f98
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-11-06 17:39:44 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (53.86%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<string,char>mp;
+        unordered_map<char,string>mp2;
+        vector<string>v;
+        int i;
+        int n = s.size();
+        string temp = "";
+        for(i=0;i<n;i++)
+        {
+            if(s[i] != ' ')
+            {
+                temp += s[i];
+            }
+            else
+            {
+                v.push_back(temp);
+                temp = "";
+            }
+        }
+        v.push_back(temp);
+        n = v.size();
+        if( n != pattern.size())
+        {
+            return false;
+        }
+        for(i=0;i<n;i++)
+        {
+          if(mp.find(v[i]) != mp.end())
+          {
+              if(mp[v[i]] == pattern[i])
+              {
+                  continue;
+              }
+              else
+              {
+                return false;
+              }
+          }
+          else
+          {
+              mp[v[i]] = pattern[i];
+          }  
+        }
+        for(i=0;i<n;i++)
+        {
+          if(mp2.find(pattern[i]) != mp2.end())
+          {
+              if(mp2[pattern[i]] == v[i])
+              {
+                  continue;
+              }
+              else
+              {
+                return false;
+              }
+          }
+          else
+          {
+              mp2[pattern[i]] = v[i];
+          }  
+        }
+        return true;
+    }
+};
