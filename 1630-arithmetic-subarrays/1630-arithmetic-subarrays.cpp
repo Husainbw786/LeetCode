076c299795fa87dc6c306346df0793a267fa77cc
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-11-08 10:46:35 +0530

    Time: 80 ms (86.45%), Space: 30.8 MB (52.80%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    bool isValid(vector<int>temp)
+    {
+        sort(temp.begin(),temp.end());
+        int x = temp[1] - temp[0];
+        for(int j=2;j<temp.size();j++)
+        {
+            if(temp[j] - temp[j-1] != x)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        
+        vector<bool>ans;
+        int n = l.size();
+        int i;
+        for(i=0;i<n;i++)
+        {
+            vector<int>temp(nums.begin()+l[i],nums.begin()+r[i]+1);
+            if(isValid(temp))
+            {
+                ans.push_back(true);
+            }
+            else
+            {
+                ans.push_back(false);
+            }
+        }
+        return ans;
+    }
+};
