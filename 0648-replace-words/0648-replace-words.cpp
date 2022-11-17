602f58fbe157319b97d288aa59e6b3722c658e8a
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-11-17 19:27:08 +0530

    Time: 373 ms (33.13%), Space: 33.3 MB (97.96%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string s) {
+        
+        int n = s.size();
+        int m = dictionary.size();
+        int i;
+        int mini = INT_MAX;
+        vector<string>v;
+        string temp = "";
+        for(i=0;i<n;i++)
+        {
+            if(s[i] != ' ')
+            {
+                temp += s[i];
+            }
+            else
+            {
+                v.push_back(temp);
+                temp = "";
+            }
+        }
+        v.push_back(temp);
+        vector<string>ans;
+        for(i=0;i<v.size();i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(v[i].rfind(dictionary[j],0) == 0)
+                {
+                    v[i] = dictionary[j];
+                }
+            }
+        }
+        s.clear();
+        for(i=0;i<v.size();i++)
+        {
+            s += v[i];
+            s += ' ';
+        }
+        s.pop_back();
+        return s;
+    }
+};
