f0ce1d034df9e503033b0eca5c8eafaa5819196e
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-11-22 19:25:30 +0530

    Time: 4 ms (56.83%), Space: 6.1 MB (76.14%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -2,48 +2,21 @@ class Solution {
 public:
     int countGoodSubstrings(string s) {
         
+        int i;
         int n = s.size();
-        int i = 0;
-        int j = 2;
-        vector<string>st;
-        vector<string>ans;
+        if(n < 3)
+        {
+            return 0;
+        }
         int count = 0;
         for(i=0;i<n-2;i++)
         {
-            string temp = "";
-            int x = i;
-            while(i <= j)
+            if(s[i] != s[i+1] && s[i+1] != s[i+2] && s[i] != s[i+2])
             {
-                temp += s[i];
-                 i++;
+                count++;
             }
-            st.push_back(temp);
-            j++;
-            i = x ;
         }
-        for(auto x : st)
-        {
-            string y = x;
-            int count_arr[26] = {0};
-            for(int j=0; j<y.size(); j++)
-            {
-                count_arr[y[j] - 'a']++;
-            }
-            bool flag = false;
-            for(i=0;i<26;i++)
-            {
-                if(count_arr[i] > 1)
-                {
-                    flag = true;
-                    break;
-                }
-            }
+        return count;
             
-            if(!flag)
-            {
-                ans.push_back(y);
-            }
-        }
-        return ans.size();
     }
 };
