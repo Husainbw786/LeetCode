528409909505d449837925445c274038c5da0dec
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-12-20 11:03:47 +0530

    Added solution - LeetHub

---
+++ Convert to Roman No - GFG/convert-to-roman-no.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    string convertToRoman(int num) {
+        // code here
+         
+        string s = "";
+        int i;
+        while(num >= 1000)
+        {
+            num = num - 1000;
+            s += "M";
+        }
+        while(num >= 900)
+        {
+            num = num - 900;
+            s += "CM";
+        }
+        while(num >= 500)
+        {
+            num = num - 500;
+            s += "D";
+        }
+        while(num >= 400)
+        {
+            num = num - 400;
+            s += "CD";
+        }
+        while(num >= 100)
+        {
+            num = num - 100;
+            s += "C";
+        }
+        while(num >= 90)
+        {
+            num = num - 90;
+            s += "XC";
+        }
+        while(num >= 50)
+        {
+            num = num - 50 ;
+            s += "L";
+        }
+        while(num >= 40)
+        {
+            num = num - 40;
+            s += "XL";
+        }
+        while(num >= 10)
+        {
+            num = num - 10;
+            s += "X";
+        }
+        while(num >= 9)
+        {
+            num = num - 9;
+            s += "IX";
+        }
+        while(num >= 5)
+        {
+            num = num - 5;
+            s += "V";
+        }
+        while(num >= 4)
+        {
+            num = num - 4;
+            s += "IV";
+        }
+        while(num >= 1)
+        {
+            num = num - 1;
+            s += "I";
+        }
+        return s;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int N;
+		cin>>N;
+		Solution ob;
+    	cout << ob.convertToRoman(N) << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
