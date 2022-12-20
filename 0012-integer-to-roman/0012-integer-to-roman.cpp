d73432f07d53f9149c650a0827023a9dd0a3858c
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-12-20 11:02:35 +0530

    Time: 8 ms (83.16%), Space: 6 MB (46.84%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        string s = "";
+        int i;
+        while(num >= 1000)
+        {
+            num = num - 1000;
+            s += "M";
+        }
+        while(num >= 900)
+        {
+            num = num - 900;
+            s += "CM";
+        }
+        while(num >= 500)
+        {
+            num = num - 500;
+            s += "D";
+        }
+        while(num >= 400)
+        {
+            num = num - 400;
+            s += "CD";
+        }
+        while(num >= 100)
+        {
+            num = num - 100;
+            s += "C";
+        }
+        while(num >= 90)
+        {
+            num = num - 90;
+            s += "XC";
+        }
+        while(num >= 50)
+        {
+            num = num - 50 ;
+            s += "L";
+        }
+        while(num >= 40)
+        {
+            num = num - 40;
+            s += "XL";
+        }
+        while(num >= 10)
+        {
+            num = num - 10;
+            s += "X";
+        }
+        while(num >= 9)
+        {
+            num = num - 9;
+            s += "IX";
+        }
+        while(num >= 5)
+        {
+            num = num - 5;
+            s += "V";
+        }
+        while(num >= 4)
+        {
+            num = num - 4;
+            s += "IV";
+        }
+        while(num >= 1)
+        {
+            num = num - 1;
+            s += "I";
+        }
+        return s;
+    }
+};
