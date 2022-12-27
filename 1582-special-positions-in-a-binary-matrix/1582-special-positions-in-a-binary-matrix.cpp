7da8ed84ad385b85fc9e814c280be3838029aac7
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-12-27 17:32:57 +0530

    Time: 26 ms (81.14%), Space: 12.7 MB (99.34%) - LeetHub

---
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        
+        int n = mat.size();
+        int m = mat[0].size();
+        int i,j;
+        int count = 0;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(mat[i][j] == 1)
+                {
+                    int x = i+1;
+                    int y = j+1;
+                    int p = i-1;
+                    int q = j-1;
+                    bool flag = false;
+                    while(x < n)
+                    {
+                        if(mat[x][j] == 1)
+                        {
+                           flag = true;
+                           break;
+                        }
+                        x++;
+                    }
+                    while(p >= 0)
+                    {
+                        if(mat[p][j] == 1)
+                        {
+                           flag = true;
+                           break;
+                        }
+                        p--;
+                    }
+                    while(y < m)
+                    {
+                        if(mat[i][y] == 1)
+                        {
+                           flag = true;
+                           break;
+                        }
+                        y++;
+                    }
+                    while(q >= 0)
+                    {
+                        if(mat[i][q] == 1)
+                        {
+                           flag = true;
+                           break;
+                        }
+                        q--;
+                    }
+                    if(!flag)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
