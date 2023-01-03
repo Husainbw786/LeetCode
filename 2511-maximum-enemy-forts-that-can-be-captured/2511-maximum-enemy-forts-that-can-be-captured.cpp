a7b9159945719c2c5d1fedbd763c49e9c24be5a5
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-01-03 10:16:47 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (15.72%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
@@ -3,51 +3,66 @@ public:
     int captureForts(vector<int>& forts) {
         int i;
         int n = forts.size();
-        int maxi = 0;
+        vector<int>v;
+        bool flag = false;
         for(i=0;i<n;i++)
         {
-            int count = 0;
             if(forts[i] == 1)
-            {    
-                for(int j=i+1;j<n;j++)
-                {
-                    if(forts[j] == 0)
-                    {
-                        count++;
-                    }
-                    else if(forts[j] == -1)
-                    {
-                        maxi = max(count,maxi);
-                        break;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
+            {
+                v.push_back(i);
             }
-            count = 0;
             if(forts[i] == -1)
-            {    
-                for(int j=i+1;j<n;j++)
-                {
-                    if(forts[j] == 0)
-                    {
-                        count++;
-                    }
-                    else if(forts[j] == 1)
-                    {
-                        maxi = max(count,maxi);
-                        break;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
+            {
+                flag = true;
             }
         }
-        
+        if(!flag)
+        {
+            return 0;
+        }
+        int maxi = 0;
+        for(i=0;i<v.size();i++)
+        {
+            int x = v[i] - 1;
+            int y = v[i] + 1;
+            int count = 0;
+            while(x >= 0)
+            {
+                if(forts[x] == 0)
+                {
+                    count++;
+                }
+                else if(forts[x] == -1)
+                {
+                    maxi = max(count,maxi);
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+                x--;
+            }
+            count = 0;
+            while(y < n)
+            {
+                if(forts[y] == 0)
+                {
+                    count++;
+                }
+                else if(forts[y] == -1)
+                {
+                    maxi = max(count,maxi);
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+                y++;
+            }
+            
+        }
         return maxi;
     }
 };
