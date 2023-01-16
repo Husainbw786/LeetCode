b1d98b51404f120e0635d060afd43021a5b7cec1
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-01-16 23:22:19 +0530

    Added solution - LeetHub

---
+++ Max distance between same elements - GFG/max-distance-between-same-elements.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // your task is to complete this function
+    int maxDistance(int arr[], int n)
+    {
+    //Code here
+    int i;
+    int ans=0;
+    unordered_map<int,int>mp;
+    for(i=0;i<n;i++)
+    {
+        if(mp.find(arr[i])!=mp.end())
+        {
+            int diff = i-mp[arr[i]];
+            if(diff>ans)
+            {
+                ans=diff;
+            }
+        }
+        else
+        {
+        mp[arr[i]]=i;
+        }
+    }
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.maxDistance(arr,n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
