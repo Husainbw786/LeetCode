b02e458aef127a4a929fa2983a80eada50ceac92
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-01-17 11:58:05 +0530

    Time: 4 ms (43.73%), Space: 6 MB (17.87%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        
+        if(n % 2 == 0)
+        {
+            return (n*n) / 4;
+        }
+        return (n/2) * (n+1)/2;
+    }
+};
