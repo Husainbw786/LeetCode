17768c7c350de7edc4d689f79d1d233d09e6e1f2
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-01-19 10:32:27 +0530

    Added solution - LeetHub

--- G.F Series - GFG/g.f-series.cpp
+++ G.F Series - GFG/g.f-series.cpp
@@ -7,20 +7,33 @@ using namespace std;
 class Solution
 {
 public:
+
+    long long int solve(int N)
+    {
+        if(N == 0)
+        {
+            return 0;
+        }
+        if(N == 1)
+        {
+            return 1;
+        }
+        long long int x = solve(N-2);
+        long long int y = solve(N-1);
+        x = x*x;
+        x = x - y;
+        return x;
+    }
+   
+
+
     void gfSeries(int N)
     {
         // Write Your Code here
-        long long int dp[N];
         int i;
-        dp[0] = 0;
-        dp[1] = 1;
-        for(i=2;i<N;i++)
-        {
-            dp[i] = (dp[i-2]*dp[i-2]) - dp[i-1];
-        }
         for(i=0;i<N;i++)
         {
-            cout << dp[i] << " ";
+            cout << solve(i) << " ";
         }
         cout << endl;
     }
