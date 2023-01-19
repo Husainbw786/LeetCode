e909de67934a983208eac4b0abb87c103185de6e
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-01-19 17:39:11 +0530

    Added solution - LeetHub

---
+++ Finding Position - GFG/finding-position.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int nthPosition(long long int n){
+        // code here
+       long long int count = 1;
+       while(n != 1)
+       {
+           count = 2 * count;
+           n = n / 2;
+       }
+       return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        Solution ob;
+        cout<<ob.nthPosition(n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
