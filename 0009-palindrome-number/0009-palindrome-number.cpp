1ab38f816cb0bb96bd8fc75fc1ffa5c5286804e5
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-01-20 11:15:42 +0530

    Time: 19 ms (58.28%), Space: 6 MB (54.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
+    bool flag = false;
+    void solve(string s, int low ,int high)
+    {
+        if(low >= high)
+        {
+            flag = true;
+            return;
+        }
+        if(s[low] != s[high])
+        {
+            return;
+        }
+        solve(s,low+1,high-1);
+        
+    }
+    
     bool isPalindrome(int x) {
         int i;
         string str = to_string(x);
-        for(i=0;i<str.length()/2;i++)
+        int low = 0;
+        int high = str.size()-1;
+        solve(str,low,high);
+        if(flag)
         {
-            if(str[i] != str[str.length()-i-1])
-                return false;
-                
+            return true;
         }
-        return true;
+        return false;
         
     }
 };
