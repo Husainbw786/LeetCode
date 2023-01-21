adf287f1ff57532a85fc382d227ca9684f9fee88
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-01-21 18:01:02 +0530

    Time: 9 ms (19.15%), Space: 8.1 MB (32.61%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    vector<int>temp;
+    void solve(int n,vector<int>&freq,vector<int>nums,vector<vector<int>>&ans)
+    {
+        if(temp.size() == n)
+        {
+           ans.push_back(temp);
+           return;
+        }
+        
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(freq[i] == 0)
+            {
+                temp.push_back(nums[i]);
+                freq[i] = 1;
+                solve(n,freq,nums,ans);
+                temp.pop_back();
+                freq[i] = 0;
+            }
+        }
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int>freq(n,0);
+        vector<vector<int>>ans;
+        solve(n,freq,nums,ans); 
+       
+        return ans;
+    }
+};
