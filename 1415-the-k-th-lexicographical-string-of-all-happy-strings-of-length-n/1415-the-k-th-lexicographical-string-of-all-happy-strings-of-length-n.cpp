507fdd24d0a37d87912232f43c25453dde83ae66
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-01-22 10:25:53 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (65.98%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string>v;
+    
+    void solve(int idx,int n,string s,string& temp,int k)
+    {
+        if(idx >= n)
+        {
+            v.push_back(temp);
+            return;
+        }
+        for(int i=0;i<3;i++)
+        {
+            if(temp.back() != s[i] && v.size() < k)
+            {
+                temp += s[i];
+                solve(idx+1,n,s,temp,k);
+                temp.pop_back();
+            }
+        }
+    }
+    
+    string getHappyString(int n, int k) {
+      
+        string s = "abc";
+        string temp = "";
+        solve(0,n,s,temp,k);
+        if(k > v.size())
+           {
+               return "";
+           }
+        return v[k-1];
+    } 
+};
