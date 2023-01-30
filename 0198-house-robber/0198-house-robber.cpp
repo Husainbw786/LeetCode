24f39af31d20eec57b292c919aec0bdeddfdc2d2
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-01-30 20:44:22 +0530

    Time: 5 ms (24.92%), Space: 7.9 MB (6.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    
-    int solve(int idx, vector<int>&nums, vector<int>&dp)
-    {
-        if(idx == 0)
-        {
-            return nums[idx];
-        }
-        if(idx < 0)
-        {
-            return 0;
-        }
-        if(dp[idx] != -1)
-        {
-            return dp[idx];
-        }
-        int pick = nums[idx] + solve(idx - 2,nums,dp);
-        int not_pick = 0 + solve(idx-1,nums,dp);
-        return dp[idx] = max(not_pick,pick);
-    }
-    
     int rob(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int>dp(n,-1);
-        return solve(n-1,nums,dp);
+        vector<int>dp(n);
+        int i;
+        dp[0] = nums[0];
+        for(i=1;i<n;i++)
+        {
+            int take = nums[i];
+            if(i > 1)
+            {
+                take += dp[i-2];
+            }
+            int not_take = 0 + dp[i-1];
+            dp[i] = max(take,not_take);
+        }
+        return dp[n-1];
     }
 };
