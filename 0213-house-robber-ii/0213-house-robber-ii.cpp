0d7c055b98a83c747741745161e89b93ae26a9bf
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-02-01 19:27:55 +0530

    Time: 3 ms (47.85%), Space: 8.1 MB (7.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,57 +1,56 @@
 class Solution {
 public:
-   
     int rob(vector<int>& nums) {
-        vector<int> v1,v2;
+        
         int n = nums.size();
         if(n==1) 
         {
             return nums[0];
         }
-        vector<int> dp1(n-1,0);
-        vector<int>dp2(n-1,0);
-        for(int i=0;i<n;i++)
+        vector<int>v1,v2;
+        vector<int>dp1(n);
+        vector<int>dp2(n);
+        int i;
+        
+        for(i=0;i<n;i++)
         {
-            if(i==0) 
-            {   
-               v1.push_back(nums[i]);
+            if(i==0)
+            {
+                v1.push_back(nums[i]);
+                continue;
             }
-            else if(i == n-1) 
+            if(i==n-1)
             {
                 v2.push_back(nums[i]);
+                continue;
             }
-            else
-            {
-                 v2.push_back(nums[i]);
-                 v1.push_back(nums[i]);
-            }
+            v1.push_back(nums[i]);
+            v2.push_back(nums[i]);
         }
-
+         
         dp1[0] = v1[0];
-        
-        for(int i=1;i<n-1;i++)
+        for(i=1;i<n-1;i++)
         {
-            int notpick = 0 + dp1[i-1];
-            int pick = v1[i] ;
+            int pick = v1[i];
             if(i>1)
             {
-                pick +=  dp1[i-2];
+                pick += dp1[i-2];
             }
-            dp1[i] = max(pick,notpick);
+            int not_pick = dp1[i-1];
+            dp1[i] = max(pick,not_pick);
         }
+        
         dp2[0] = v2[0];
-        for(int i=1;i<n-1;i++)
+        for(i=1;i<n-1;i++)
         {
-            int notpick = 0 + dp2[i-1];
             int pick = v2[i];
             if(i>1)
             {
-                pick +=  dp2[i-2];
+                pick += dp2[i-2];
             }
-            dp2[i] = max(pick,notpick);
+            int not_pick = dp2[i-1];
+            dp2[i] = max(pick,not_pick);
         }
-
-
-        return max( dp1[n-2] , dp2[n-2] );
+        return max(dp1[n-2],dp2[n-2]);
     }
 };
