1f25e701cd6271377cac3f3981743b2b8cec67f5
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-04-03 10:47:26 -0700

    Time: 15 ms (12.50%), Space: 30 MB (12.50%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -5,13 +5,15 @@ public:
         unordered_map<int,int>mp;
         int n = nums.size();
         int i;
+        int maxi = 0;
         for(i=0;i<n;i++)
         {
             mp[nums[i]]++;
+            maxi = max(mp[nums[i]],maxi);
         }
         vector<vector<int>>ans;
         int count = n;
-        while(count--)
+        while(maxi--)
         {
             vector<int>temp;
             for(auto x : mp)
@@ -21,11 +23,6 @@ public:
                     temp.push_back(x.first);
                     mp[x.first]--;
                 }
-            }
-
-            if(temp.size() == 0)
-            {
-                break;
             }
             ans.push_back(temp);
         }
