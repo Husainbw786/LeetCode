d1a4ea85884c001a3f4be39da2d358f5161cbd59
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-04-08 22:59:14 -0700

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,46 +6,45 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    
-    bool dfs(int node, vector<int>&vis,vector<int>&path,vector<int>adj[])
-    {
-        vis[node] = 1;
-        path[node] = 1;
-        for(auto x : adj[node])
-        {
-            if(!vis[x])
-            {
-                if(dfs(x,vis,path,adj) == true)
-                {
-                    return true;
-                }
-            }
-            else if(path[x])
-            {
-                return true;
-            }
-        }
-        path[node] = 0;
-        return false;
-        
-    }
-    
-    
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        vector<int>vis(V,0);
-        vector<int>path(V,0);
-        for(int i=0;i<V;i++)
+        vector<int>indegree(V,0);
+        vector<int>topo;
+        int i;
+        for(i=0;i<V;i++)
         {
-            if(!vis[i])
+            for(auto x : adj[i])
             {
-                if(dfs(i,vis,path,adj) == true)
+                indegree[x]++;
+            }
+        }
+        queue<int>q;
+        for(i=0;i<V;i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+        while(!q.empty())
+        {
+            int front  = q.front();
+            q.pop();
+            topo.push_back(front);
+            for(auto x : adj[front])
+            {
+                indegree[x]--;
+                if(indegree[x] == 0)
                 {
-                    return true;
+                    q.push(x);
                 }
             }
         }
-        return false;
+        if(topo.size() == V)
+        {
+            return false;
+        }
+        return true;
     }
 };
 
