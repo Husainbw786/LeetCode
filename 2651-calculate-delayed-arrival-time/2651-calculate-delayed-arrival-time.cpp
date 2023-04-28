1ae67d92b8397875f33dbc5763ea0513fad9e117
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-04-28 06:06:45 -0700

    Time: 0 ms (100.00%), Space: 6 MB (5.54%) - LeetHub

--- 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.cpp
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
         
-        if(arrivalTime + delayedTime < 24)
-        {
-            return arrivalTime + delayedTime;
-        }
+       
         int x = (arrivalTime + delayedTime) % 24;
+        
         return x;
     }
 };
