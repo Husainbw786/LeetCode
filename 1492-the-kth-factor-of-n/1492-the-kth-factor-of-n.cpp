d9a8c10fe7ecf25ef774d8a05f963ca5b7516bed
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-05-18 09:20:22 -0700

    Time: 0 ms (100.00%), Space: 6 MB (73.18%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -3,19 +3,32 @@ public:
     int kthFactor(int n, int k) {
         
         int i;
-        int x = k;
-    
-        for(i=1;i<=n;i++)
+        vector<int>v;
+        if(n == 1 && k == 1)
         {
-            if(n % i == 0)
+            return 1;
+        }
+        
+        for(i=1;i<=sqrt(n);i++)
+        {
+            if(n % i == 0 && i*i != n)
             {
-                x--;
-                if(x == 0)
-                {
-                    return i;
-                }
+               v.push_back(i);
+               v.push_back(n/i);
+            }
+            else if(n % i == 0)
+            {
+                v.push_back(i);
             }
         }
-        return -1;
+        
+        sort(v.begin(),v.end());
+        
+        if(v.size() < k)
+        {
+           return -1; 
+        }
+        return v[k-1];
+        
     }
 };
