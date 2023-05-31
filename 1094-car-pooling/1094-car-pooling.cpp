1ecfb7495b6be29054dab90ebefb2768ca7c4973
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-05-31 20:47:24 +0530

    Time: 18 ms (46.43%), Space: 10.1 MB (92.41%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -3,15 +3,21 @@ public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         
         int n = trips.size();
-        vector<int>v(1001,0);
-        int i;
+        int maxi = 0;
+         int i;
+        for(int i=0;i<n;i++)
+        {
+            maxi = max(trips[i][2],maxi);
+        }
+        vector<int>v(maxi+1,0);
+      
         for(i=0;i<n;i++)
         {
             v[trips[i][1]] += trips[i][0];
             v[trips[i][2]] -= trips[i][0];
         }
         int count = 0; 
-        for(i=0;i<1001;i++)
+        for(i=0;i<maxi+1;i++)
         {
             count += v[i];
             if(count > capacity)
