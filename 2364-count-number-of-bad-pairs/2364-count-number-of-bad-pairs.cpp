db851b8c3bc4a408784820a4c66c14705b18574a
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-06-03 21:31:49 +0530

    Time: 245 ms (50.00%), Space: 83.6 MB (95.04%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -6,12 +6,14 @@ public:
          long long int n = nums.size();
          unordered_map<int,int>mp;
          int i;
+        
          for(i=0;i<n;i++)
          {
             int curr = nums[i] - i;
             good  = good + mp[curr];
             mp[curr]++;
          }
+          
         long long total = n*(n-1) /2;
         
         return total - good; 
