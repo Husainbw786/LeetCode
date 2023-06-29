b305ad429572ea9a34f45c5a856a8fc78dc658c0
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-06-29 18:43:23 +0530

    Added solution - LeetHub

---
+++ Segregate even and odd nodes in a Link List - GFG/segregate-even-and-odd-nodes-in-a-link-list.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head){
+        // code here
+        Node* odd = new Node(0);
+        Node* even = new Node(0);
+        Node* odd_ref = odd;
+        Node* even_ref = even;
+        
+        while(head)
+        {
+            if(head->data % 2 == 0)
+            {
+                even->next = head;
+                even = even->next;
+            }
+            else
+            {
+                odd->next = head;
+                odd = odd->next;
+            }
+            head = head->next;
+        }
+        odd->next = NULL;
+        even->next = odd_ref->next;
+        return even_ref->next;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
+
+// } Driver Code Ends
