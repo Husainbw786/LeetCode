f92a1a74376e499146bc360bfcfd68e2455af969
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-09-04 12:15:30 +0530

    Time: 2056 ms (9.45%), Space: 6 MB (79.76%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int solve(int n)
+    {
+        if(n <= 1)
+        {
+            return 1;
+        }
+        int ans = 0;
+        for(int i=1;i<=n;i++)
+        {
+            ans += solve(i-1) * solve(n-i);
+        }
+        return ans;
+    }
+    
+    int numTrees(int n) {
+        
+        return solve(n);
+    }
+};
