ab809c30a952dcb378682e179afa7ee6755687b8
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-09-22 11:40:01 +0530

    Time: 153 ms (63.77%), Space: 94.2 MB (51.71%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2780-minimum-index-of-a-valid-split/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2780-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        
+        int n = nums.size();
+        if(n == 1)
+        {
+            return -1;
+        }
+        int i;
+        unordered_map<int,int>mp;
+        int maxi = INT_MIN;
+        int dom = -1;
+        for(i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+            if(maxi < mp[nums[i]])
+            {
+                maxi = mp[nums[i]];
+                dom = nums[i];
+            }
+        }
+
+        int count = 0;
+        
+        for(i=0;i<n;i++)
+        {
+            if(nums[i] == dom)
+            {
+                count++;
+                if(count*2 > i+1)
+                {
+                    if((mp[dom] - count)*2  > n-i-1)
+                    {
+                        return i;
+                    }
+                }
+            }
+        }
+     return -1;
+    }
+};
