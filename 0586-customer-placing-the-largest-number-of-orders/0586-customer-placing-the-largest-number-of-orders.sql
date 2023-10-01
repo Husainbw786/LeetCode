2e948d5d724bda523281295a4ee49e3b5c08f899
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-10-01 19:21:48 +0530

    Time: 880 ms (90.41%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-SELECT customer_number from orders group by customer_number order by COUNT(customer_number) DESC limit 1
+SELECT customer_number from orders group by customer_number order by COUNT(customer_number) DESC LIMIT 1
