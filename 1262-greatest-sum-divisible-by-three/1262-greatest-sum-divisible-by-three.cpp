eed65dd782af06084a2202916925b03a678d9642
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-11-16 12:50:27 +0530

    Time: 66 ms (52.35%), Space: 42.4 MB (19.13%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -3,20 +3,20 @@ public:
     
     int solve(int i,int mod,vector<int>&nums,vector<vector<int>>&dp)
     {
-        if(i == nums.size())
+        if(i < 0)
         {
             if(mod == 0)
             {
                 return 0;
             }
-            return -1e6;
+            return -1e7;
         }
         if(dp[i][mod] != -1)
         {
             return dp[i][mod];
         }
-        int pick = nums[i] + solve(i+1,(mod+nums[i]) % 3,nums,dp);
-        int not_pick = solve(i+1,mod,nums,dp);
+        int pick = nums[i] + solve(i-1,(mod+nums[i]) % 3,nums,dp);
+        int not_pick = solve(i-1,mod,nums,dp);
         return dp[i][mod] = max(pick,not_pick);
     }
     
@@ -25,9 +25,7 @@ public:
         int n = nums.size();
         int mod = 0;
         vector<vector<int>>dp(n,vector<int>(3,-1));
-        return solve(0,0,nums,dp);
+        return solve(n-1,mod,nums,dp);
             
     }
 };
-
-
