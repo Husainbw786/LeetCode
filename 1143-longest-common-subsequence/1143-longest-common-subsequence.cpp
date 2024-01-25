cda9f0988302b8ebd6c9b03d5e7bc4db38dd9695
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-01-25 16:15:13 +0530

    Time: 10 ms (99.79%), Space: 11.8 MB (72.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,7 +4,9 @@ public:
         
         int n = text1.size();
         int m = text2.size();
-        int i,j;
+
+        int i;
+        int j;
         int dp[n+1][m+1];
         for(i=0;i<=n;i++)
         {
@@ -27,6 +29,8 @@ public:
                 }
             }
         }
+
         return dp[n][m];
+
     }
 };
