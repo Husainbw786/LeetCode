5590fa604e21831b944cc45222d2f9107080ee0c
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-03-19 16:18:19 +0530

    Time: 87 ms (37.59%), Space: 52.7 MB (9.96%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         
-        unordered_map<char, int> mp;
+        unordered_map<char,int> mp;
         int i;
         
-        for (i = 0; i < tasks.size(); i++) {
+        for (i = 0; i < tasks.size(); i++) 
+        {
             mp[tasks[i]]++;
         }
-        
+
         int ans = 0;
         priority_queue<pair<int, char>> pq;
         
-        for (auto x : mp) {
-            pq.push({x.second, x.first});
+        for (auto x : mp) 
+        {
+          pq.push({x.second, x.first});
         }
         
-        while (!pq.empty()) {
+        while (!pq.empty()) 
+        {
             vector<pair<int, char>> temp;
             int k = n + 1;
             
@@ -26,14 +29,16 @@ public:
                 int count = pq.top().first;
                 pq.pop();
                 
-                if (count > 1) {
+                if (count > 1) 
+                {
                     temp.push_back({count - 1, x});
                 }
                 k--;
                 ans++;
             }
             
-            for (auto p : temp) {
+            for (auto p : temp) 
+            {
                 pq.push(p);
             }
             
@@ -41,7 +46,7 @@ public:
                 break;
             }
             
-            ans += k; // Add idle slots if necessary
+            ans += k; 
         }
         return ans;
         
